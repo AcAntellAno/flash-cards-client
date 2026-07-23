cc08917dd784d280f6fdf8881ff190f356fb6ee1
Anthony Cantellano <acantellano1996@gmail.com>
2026-07-22 21:55:54 -0700

    feat(LEA-33): styled the modal component

--- src/components/cards/Modal.tsx
+++ src/components/cards/Modal.tsx
@@ -1,9 +1,41 @@
 import { useState, useRef, useEffect } from 'react';
+import styled from 'styled-components';
 
 interface IModal {
   buttonText: string;
   children: React.ReactNode;
 }
+
+const ModalContainer = styled.section`
+  max-width: 90vw;
+  max-height: 90vw;
+`;
+
+const StyledModal = styled.dialog`
+  height: 10vw;
+  // below line could create issues since we are overwritting default behavior of dialog modal
+  // this allows our X button to close the modal to display on top right corner of modal
+  // this default behavior probably creates scrollable content, but in our case we dont care, so should be fine
+  overflow: visible;
+  &::backdrop {
+    background-color: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(4px);
+  }
+`;
+
+const ModalContents = styled.div`
+  position: relative;
+  z-index: 0;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: -1.5rem;
+  right: -1.5rem;
+  z-index: 1;
+  border-radius: 15px;
+  border-color: transparent;
+`;
 
 const Modal = (props: IModal) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,12 +46,23 @@ const Modal = (props: IModal) => {
     dialogRef.current?.showModal();
   }, [isOpen]);
 
+  const closeModal = () => {
+    dialogRef.current?.close();
+    setIsOpen(!isOpen);
+  };
   const { children, buttonText } = props;
   return (
-    <section>
+    <ModalContainer>
       <button onClick={() => setIsOpen(!isOpen)}>{buttonText}</button>
-      <dialog ref={dialogRef}>{isOpen && <>{children}</>}</dialog>
-    </section>
+      <StyledModal ref={dialogRef}>
+        {isOpen && (
+          <ModalContents>
+            <CloseButton onClick={() => closeModal()}>X</CloseButton>
+            {children}
+          </ModalContents>
+        )}
+      </StyledModal>
+    </ModalContainer>
   );
 };
 
